3a78a4895388e39ad1f3664f16a14d9372f4ad1e
23BCE1956 <archishman.debnath2023@vitstudent.ac.in>
2026-07-30 15:44:49 +0530

    feat: v0.1 portfolio foundation

---
+++ src/config/site.ts
@@ -0,0 +1,19 @@
+export const site = {
+    name: "Archishman Devnath",
+  
+    role: "Aspiring Product Manager",
+  
+    tagline: "From curiosity to clarity.",
+  
+    description:
+      "I break down products I admire, build experiments, and document my learnings on product strategy, user experience, and business decisions.",
+    
+    //links
+    links: {
+        resume: "#",
+        projects: "#",
+        email: "archishman.devnath@gmail.com",
+        github: "https://github.com/biblomaniac",
+        linkedin: "https://www.linkedin.com/in/archishman-debnath-8061b6293/",
+    },
+  };
